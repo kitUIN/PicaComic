838fb732bd68444ce6adb2c9b75579927a12f517
Nacho <kulujun@gmail.com>
2023-08-11 22:45:59 +0800

    子评论

--- PicaComic/Models/Comic.cs
+++ PicaComic/Models/Comic.cs
@@ -1,97 +1,101 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using Windows.Services.Maps.LocalSearch;
 
-namespace PicaComic.Models
-{
-    /// <summary>
-    /// 漫画
-    /// </summary>
-    public class Comic: ObservableObject
-    {
-        #region private
-        private bool isLocked;
-        private string lockCategoryString;
-        private List<string> lockCategories;
-        #endregion
-        /// <summary>
-        /// ID
-        /// </summary>
-        [JsonPropertyName("_id")]
-        public string Id { get; set; }
-        /// <summary>
-        /// 漫画名
-        /// </summary>
-        [JsonPropertyName("title")]
-        public string Title { get; set; }
-        /// <summary>
-        /// 作者
-        /// </summary>
-        [JsonPropertyName("author")]
-        public string Author { get; set; }
-        /// <summary>
-        /// 是否完结
-        /// </summary>
-        [JsonPropertyName("finished")]
-        public bool Finished { get; set; }
-        /// <summary>
-        /// 分类
-        /// </summary>
-        [JsonPropertyName("categories")]
-        public List<string> Categories { get; set; }
+namespace PicaComic.Models;
 
-        /// <summary>
-        /// 缩略图
-        /// </summary>
-        [JsonPropertyName("thumb")]
-        public Thumb Thumb { get; set; }
-        /// <summary>
-        /// 分类转string
-        /// </summary>
-        [JsonIgnore]
-        public string CategoryString => Categories.ListString();
-        /// <summary>
-        /// 是否被封印
-        /// </summary>
-        [JsonIgnore]
-        public bool IsLocked 
-        { 
-            get => isLocked; 
-            set
-            {
-                if(IsLocked != value)
-                {
-                    SetProperty(ref isLocked, value);
-                }
-            }
-        }
-        /// <summary>
-        /// 被封印的分区
-        /// </summary>
-        [JsonIgnore]
-        public List<string> LockCategories 
+/// <summary>
+/// 漫画
+/// </summary>
+public partial class Comic : ObservableObject
+{
+    #region private
+
+    private bool isLocked;
+    private string lockCategoryString;
+    private List<string> lockCategories;
+
+    #endregion
+
+    /// <summary>
+    /// ID
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("_id")]
+    private string id;
+
+    /// <summary>
+    /// 漫画名
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("title")]
+    private string title;
+
+    /// <summary>
+    /// 作者
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("author")]
+    private string author;
+
+    /// <summary>
+    /// 是否完结
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("finished")]
+    private bool finished;
+
+    /// <summary>
+    /// 分类
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("categories")]
+    private List<string> categories;
+
+    /// <summary>
+    /// 缩略图
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("thumb")]
+    private Thumb thumb;
+
+    /// <summary>
+    /// 分类转string
+    /// </summary>
+    [JsonIgnore]
+    public string CategoryString => Categories.ListString();
+
+    /// <summary>
+    /// 是否被封印
+    /// </summary>
+    [JsonIgnore]
+    public bool IsLocked
+    {
+        get => isLocked;
+        set
         {
-            get => lockCategories;
-            set
-            {
-                if (lockCategories == value) return;
-                SetProperty(ref lockCategories, value);
-                LockCategoryString = value.ListString();
-            }
+            if (IsLocked != value) SetProperty(ref isLocked, value);
         }
-        /// <summary>
-        /// 被封印的分区文字版
-        /// </summary>
-        [JsonIgnore]
-        public string LockCategoryString 
-        { 
-            get => lockCategoryString;
-            set
-            {
-                if (LockCategoryString != value)
-                {
-                    SetProperty(ref lockCategoryString, value);
-                }
-            }
+    }
+
+    /// <summary>
+    /// 被封印的分区
+    /// </summary>
+    [JsonIgnore]
+    public List<string> LockCategories
+    {
+        get => lockCategories;
+        set
+        {
+            if (lockCategories == value) return;
+            SetProperty(ref lockCategories, value);
+            LockCategoryString = value.ListString();
+        }
+    }
+
+    /// <summary>
+    /// 被封印的分区文字版
+    /// </summary>
+    [JsonIgnore]
+    public string LockCategoryString
+    {
+        get => lockCategoryString;
+        set
+        {
+            if (LockCategoryString != value) SetProperty(ref lockCategoryString, value);
         }
     }
 }
--- PicaComic/Models/Comment.cs
+++ PicaComic/Models/Comment.cs
@@ -1,4 +1,5 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace PicaComic.Models
 {
@@ -6,41 +7,76 @@ namespace PicaComic.Models
     /// 评论
     /// </summary>
     public partial class Comment: ObservableObject
-    {
+    {   
+        /// <summary>
+        /// 评论Id
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("_id")]
         private string id ;
-
+        /// <summary>
+        /// 评论内容
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("content")]
         private string content ;
-
+        /// <summary>
+        /// 评论者
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("_user")]
         private Commenter user ;
-
+        /// <summary>
+        /// 漫画ID
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("_comic")]
         private string comic ;
-
+        /// <summary>
+        /// 评论数量
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("totalComments")]
         private int totalComments ;
-
+        /// <summary>
+        /// 是否置顶
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("isTop")]
         private bool isTop ;
-
+        /// <summary>
+        /// 是否隐藏
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("hide")]
-        private bool hide ;
-
+        private bool isHide ;
+        /// <summary>
+        /// 创建时间
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("created_at")]
         private DateTime createdAt ;
-
+        /// <summary>
+        /// 喜欢数量
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("likesCount")]
         private int likesCount ;
-
+        /// <summary>
+        /// 评论数量
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("commentsCount")]
         private int commentsCount ;
-
+        /// <summary>
+        /// 是否喜欢
+        /// </summary>
         [ObservableProperty] [property: JsonPropertyName("isLiked")]
         private bool isLiked ;
+        /// <summary>
+        /// 第几楼
+        /// </summary>
         [ObservableProperty] [property: JsonIgnore]
         private int order;
+        /// <summary>
+        /// 是否显示子评论
+        /// </summary>
+        [ObservableProperty] [property: JsonIgnore]
+        private bool isShowChildren;
+        /// <summary>
+        /// 子评论
+        /// </summary>
+        public ObservableCollection<Comment> Children { get; } = new ObservableCollection<Comment>();
     }
 
 
