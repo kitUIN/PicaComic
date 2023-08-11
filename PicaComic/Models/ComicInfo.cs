bca63a1e663516663eaffeb745931e816666d653
Nacho <kulujun@gmail.com>
2023-08-11 20:28:31 +0800

    修正

--- PicaComic/Models/ComicInfo.cs
+++ PicaComic/Models/ComicInfo.cs
@@ -1,122 +1,118 @@
-﻿using System.Collections.ObjectModel;
+using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
-namespace PicaComic.Models
+namespace PicaComic.Models;
+
+/// <summary>
+/// 漫画详细信息
+/// </summary>
+/// <seealso cref="PicaComic.Models.Comic" />
+public partial class ComicInfo : Comic
 {
     /// <summary>
-    /// 漫画详细信息
+    /// 总页数
     /// </summary>
-    /// <seealso cref="PicaComic.Models.Comic" />
-    public class ComicInfo: Comic
-    {
-        /// <summary>
-        /// 总页数
-        /// </summary>
-        [JsonPropertyName("pagesCount")]
-        public int PagesCount { get; set; }
+    [ObservableProperty] [property: JsonPropertyName("pagesCount")]
+    private int pagesCount;
 
-        /// <summary>
-        /// 总话数
-        /// </summary>
-        [JsonPropertyName("epsCount")]
-        public int EpsCount { get; set; }
-        /// <summary>
-        /// 总喜欢
-        /// </summary>
-        [JsonPropertyName("totalLikes")]
-        public int TotalLikes { get; set; }
-        /// <summary>
-        /// 总查看
-        /// </summary>
-        [JsonPropertyName("totalViews")]
-        public int TotalViews { get; set; }
-        /// <summary>
-        /// 喜欢数量
-        /// </summary>
-        [JsonPropertyName("likesCount")]
-        public int LikesCount { get; set; }
-        /// <summary>
-        /// 上传者
-        /// </summary>
-        [JsonPropertyName("_creator")]
-        public Creater Creator { get; set; }
-        /// <summary>
-        /// 介绍
-        /// </summary>
-        [JsonPropertyName("description")]
-        public string Description { get; set; }
-        /// <summary>
-        /// 汉化组
-        /// </summary>
-        [JsonPropertyName("chineseTeam")]
-        public string ChineseTeam { get; set; }
-        /// <summary>
-        /// 标签
-        /// </summary>
-        [JsonPropertyName("tags")]
-        public List<string> Tags { get; set; }
+    /// <summary>
+    /// 总话数
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("epsCount")]
+    private int epsCount;
 
-        /// <summary>
-        /// 更新时间
-        /// </summary>
-        [JsonPropertyName("updated_at")]
-        public DateTime UpdatedAt { get; set; }
-        /// <summary>
-        /// 上传时间
-        /// </summary>
-        [JsonPropertyName("created_at")]
-        public DateTime CreatedAt { get; set; }
-        /// <summary>
-        /// 允许下载
-        /// </summary>
-        [JsonPropertyName("allowDownload")]
-        public bool AllowDownload { get; set; }
-        /// <summary>
-        /// 允许评论
-        /// </summary>
-        [JsonPropertyName("allowComment")]
-        public bool AllowComment { get; set; }
-        /// <summary>
-        /// 总评论
-        /// </summary>
-        [JsonPropertyName("totalComments")]
-        public int TotalComments { get; set; }
-        /// <summary>
-        /// 查看数量
-        /// </summary>
-        [JsonPropertyName("viewsCount")]
-        public int ViewsCount { get; set; }
-        /// <summary>
-        /// 评论数量
-        /// </summary>
-        [JsonPropertyName("commentsCount")]
-        public int CommentsCount { get; set; }
-        /// <summary>
-        /// 是否收藏
-        /// </summary>
-        [JsonPropertyName("isFavourite")]
-        public bool IsFavourite { get; set; }
-        /// <summary>
-        /// 是否喜欢
-        /// </summary>
-        [JsonPropertyName("isLiked")]
-        public bool IsLiked { get; set; }
-    }
+    /// <summary>
+    /// 总喜欢
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("totalLikes")]
+    private int totalLikes;
 
+    /// <summary>
+    /// 总查看
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("totalViews")]
+    private int totalViews;
 
+    /// <summary>
+    /// 喜欢数量
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("likesCount")]
+    private int likesCount;
 
+    /// <summary>
+    /// 上传者
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("_creator")]
+    private Creater creator;
 
+    /// <summary>
+    /// 介绍
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("description")]
+    private string description;
 
+    /// <summary>
+    /// 汉化组
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("chineseTeam")]
+    private string chineseTeam;
 
+    /// <summary>
+    /// 标签
+    /// </summary>
+    [ObservableProperty][property: JsonPropertyName("tags")] private ObservableCollection<string> tags;
 
+    /// <summary>
+    /// 更新时间
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("updated_at")]
+    private DateTime updatedAt;
 
+    /// <summary>
+    /// 上传时间
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("created_at")]
+    private DateTime createdAt;
 
+    /// <summary>
+    /// 允许下载
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("allowDownload")]
+    private bool allowDownload;
 
+    /// <summary>
+    /// 允许评论
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("allowComment")]
+    private bool allowComment;
 
+    /// <summary>
+    /// 总评论
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("totalComments")]
+    private int totalComments;
 
+    /// <summary>
+    /// 查看数量
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("viewsCount")]
+    private int viewsCount;
 
+    /// <summary>
+    /// 评论数量
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("commentsCount")]
+    private int commentsCount;
 
+    /// <summary>
+    /// 是否收藏
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("isFavourite")]
+    private bool isFavourite;
 
-
-
-
+    /// <summary>
+    /// 是否喜欢
+    /// </summary>
+    [ObservableProperty] [property: JsonPropertyName("isLiked")]
+    private bool isLiked;
 }
